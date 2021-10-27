92af9091cc14313182e7ddf6abf06b7d04c3f3bc
Niklas Bülow <me@niklasbuelow.com>
2021-10-27 17:53:30 +0200

    feat: add --version argument option to display LeanInk version

--- LeanInk/Main.lean
+++ LeanInk/Main.lean
@@ -1,5 +1,13 @@
 import LeanInk
+import LeanInk.Version
+
+def printVersion : IO UInt32 := do
+  IO.println LeanInk.cliVersionOutput
+  return 1
 
 def main (args : List String) : IO UInt32 := do
-  IO.println s!"Hello from LeanInk!"
-  return 1
+  match args with
+  | ["--version"] => printVersion
+  | _ => 
+      IO.println s!"Hello from LeanInk!"
+      return 1
---
+++ LeanInk/Version.lean
@@ -0,0 +1,4 @@
+namespace LeanInk
+
+def version := s!"0.0.1-alpha"
+def cliVersionOutput := s!"LeanInk ({version})"
