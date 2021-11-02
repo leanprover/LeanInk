51aafc506e37041b0ac57a77e0da5429bc9e0262
Niklas Bülow <me@niklasbuelow.com>
2021-11-03 00:19:35 +0100

    chore: update lean-toolchain

--- LeanInk/Version.lean
+++ LeanInk/Version.lean
@@ -1,8 +1,8 @@
 namespace LeanInk
 namespace Version
 
-def version := s!"0.0.1-alpha"
-def leanVersion := s!"leanprover/lean4:nightly-2021-10-27"
+def version := s!"0.0.1"
+def leanVersion := s!"leanprover/lean4:nightly-2021-11-02"
 def cliVersionOutput := s!"LeanInk ({version}) for Lean ({leanVersion})"
 
 def printVersion : IO UInt32 := do
