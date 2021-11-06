dd60576a51a15b04a5c68a793aebbd26f2c3cd7d
Niklas Bülow <me@niklasbuelow.com>
2021-11-07 00:48:44 +0100

    feat: attempt on reading input file

--- LeanInk/Commands/Analyze/Basic.lean
+++ LeanInk/Commands/Analyze/Basic.lean
@@ -8,7 +8,16 @@ namespace LeanInk.Commands.Analyze
 
 open LeanInk.CLI
 
+def _debugMessage (file content : String) := s!"
+FILE IMPORTED ({file}):
+{content}
+"
+
 def exec (globalArgs: List GlobalArgument) (args: List String) : IO UInt32 := do
-  let (arguments, _) : List Argument × List String := parseArgumentList args
-  Logger.logInfo s!"Executing analysis for {args}"
-  return 0
+  let (arguments, files) : List Argument × List String := parseArgumentList args
+  match files with
+  | a::as =>
+    let input <- IO.FS.readFile a
+    Logger.logInfo (_debugMessage a input)
+    return 0
+  | _ => Logger.logError s!"No input files provided"
