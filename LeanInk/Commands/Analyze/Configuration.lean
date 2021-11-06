e6c87c778ee05f0404bf87d6c1f8ef72e6e9462c
Niklas Bülow <me@niklasbuelow.com>
2021-11-06 21:18:13 +0100

    feat: add analyze configuration structure

---
+++ LeanInk/Commands/Analyze/Configuration.lean
@@ -0,0 +1,16 @@
+namespace LeanInk.Commands.Analyze
+
+open System
+
+-- The `OutputType` specifies in which format the result of
+-- leanInks analysis gets returned.
+inductive OutputType where
+  -- alectryonFragments describes the format used by Alectryon which basically is a list
+  -- of fragments. Each fragment is either a `Text` or `Sentence`. TODO: specify further
+  | alectryonFragments : OutputType 
+
+-- The analyze `Configuration` describes all input specifications and infos for
+-- the LeanInk analysis execution context. It contains the list of input file paths, etc.
+structure Configuration where
+  inputFiles: List FilePath
+  outputType : OutputType
