f92e5f814ff590235aea277cd85bdb8061c34b1a
Niklas Bülow <me@niklasbuelow.com>
2021-11-07 00:24:53 +0100

    feat: retrieve lean version directly

--- LeanInk/Commands/Version.lean
+++ LeanInk/Commands/Version.lean
@@ -1,7 +1,15 @@
 namespace LeanInk.Commands.Version
 
+def leanVersion : String := do
+  let versionString := s!"{Lean.version.major}.{Lean.version.minor}.{Lean.version.patch}"
+  if Lean.version.isRelease then
+    return versionString
+  else if !String.isEmpty Lean.version.specialDesc then
+    return versionString ++ s!"-" ++ Lean.version.specialDesc
+  else
+    return versionString ++ s!"-unknown"
+
 def version := s!"0.0.1"
-def leanVersion := s!"leanprover/lean4:nightly-2021-11-02"
 def cliVersionOutput := s!"LeanInk ({version}) for Lean ({leanVersion})"
 
 def printVersion : IO UInt32 := do
@@ -11,3 +19,4 @@ def printVersion : IO UInt32 := do
 def printLeanVersion : IO UInt32 := do
   IO.println leanVersion
   return 0
+
