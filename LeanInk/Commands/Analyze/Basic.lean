20de508aeeca749f1b7da50388b33fca60d0974f
Niklas Bülow <me@niklasbuelow.com>
2021-11-07 01:06:57 +0100

    feat: check if provided file is .lean file

--- LeanInk/Commands/Analyze/Basic.lean
+++ LeanInk/Commands/Analyze/Basic.lean
@@ -7,17 +7,27 @@ import LeanInk.Commands.Analyze.Argument
 namespace LeanInk.Commands.Analyze
 
 open LeanInk.CLI
+open System
 
 def _debugMessage (file content : String) := s!"
 FILE IMPORTED ({file}):
 {content}
 "
 
+def leanFileExtension := s!"lean"
+
+def isLeanFile (path : FilePath) : Bool := do
+  return path.extension == leanFileExtension
+
 def exec (globalArgs: List GlobalArgument) (args: List String) : IO UInt32 := do
   let (arguments, files) : List Argument × List String := parseArgumentList args
   match files with
   | a::as =>
-    let input <- IO.FS.readFile a
-    Logger.logInfo (_debugMessage a input)
-    return 0
+    let path : FilePath := a
+    if isLeanFile path then
+      let input <- IO.FS.readFile a
+      Logger.logInfo (_debugMessage a input)
+      return 0
+    else
+      Logger.logError s!"Provided file \"{a}\" is not lean file."
   | _ => Logger.logError s!"No input files provided"
