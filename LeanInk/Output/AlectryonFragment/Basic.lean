2ca49de14a3ebc4e7cc8ecf5e79cd4c0df779580
Niklas Bülow <me@niklasbuelow.com>
2021-11-09 00:48:34 +0100

    feat: make fragments derive from FromJson, ToJson

--- LeanInk/Output/AlectryonFragment/Basic.lean
+++ LeanInk/Output/AlectryonFragment/Basic.lean
@@ -1,26 +1,36 @@
+import Lean.Data.Json
+
 namespace LeanInk.Output.AlectryonFragment
+
+open Lean
 
 structure Hypothesis where
   names : List String
   body : String
   type : String
+  deriving FromJson, ToJson
 
 structure Goal where
   name : String
   conclusion : String
   hyptheses : Array Hypothesis
+  deriving FromJson, ToJson
 
 structure Message where
   contents : String
+  deriving FromJson, ToJson
 
 structure Sentence where
   contents : String
   messages : Array Message
   goals : Array Goal
+  deriving FromJson, ToJson
 
 structure Text where
   contents : String
+  deriving FromJson, ToJson
 
 inductive Fragment where
-| text (value : Text) : Fragment
-| sentence (value : Sentence) : Fragment
+  | text (value : Text) : Fragment
+  | sentence (value : Sentence) : Fragment
+  deriving FromJson, ToJson
