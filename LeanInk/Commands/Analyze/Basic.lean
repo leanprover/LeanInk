99f3aa75478fa80e4d10438f5a33edc285ff5e91
Niklas Bülow <me@niklasbuelow.com>
2021-11-11 02:32:15 +0100

    feat: use lake dependency to load search paths

--- LeanInk/Commands/Analyze/Basic.lean
+++ LeanInk/Commands/Analyze/Basic.lean
@@ -4,13 +4,18 @@ import LeanInk.CLI.Logger
 
 import LeanInk.Commands.Analyze.Argument
 
+import Lean.Elab.Frontend
+import Lean.Util.Path
+
+import Lake
+
 namespace LeanInk.Commands.Analyze
 
 open LeanInk.CLI
+open Lean
 open System
 
-def _debugMessage (file content : String) := s!"
-FILE IMPORTED ({file}):
+def _debugMessage (file content : String) := s!" {file}:
 {content}
 "
 
@@ -18,6 +23,18 @@ def leanFileExtension := s!"lean"
 
 def isLeanFile (path : FilePath) : Bool := do
   return path.extension == leanFileExtension
+
+def initializeLeanContext : IO UInt32 := do
+  let lean <- Lake.findLeanInstall?
+  let lake <- Lake.findLakeInstall?
+  let res <- Lake.setupLeanSearchPath lean lake
+  return 0
+
+def loadInput (content : String) (name : FilePath) : IO UInt32 := do
+  Logger.logInfo s!"Running frontend:"
+  let i <- initializeLeanContext 
+  let (env, ok) <- Elab.runFrontend content Options.empty name.toString `Init
+  if ok then return 0 else return 1
 
 def exec (globalArgs: List GlobalArgument) (args: List String) : IO UInt32 := do
   let (arguments, files) : List Argument × List String := parseArgumentList args
@@ -27,7 +44,7 @@ def exec (globalArgs: List GlobalArgument) (args: List String) : IO UInt32 := do
     if isLeanFile path then
       let input <- IO.FS.readFile a
       Logger.logInfo (_debugMessage a input)
-      return 0
+      loadInput input path
     else
       Logger.logError s!"Provided file \"{a}\" is not lean file."
   | _ => Logger.logError s!"No input files provided"
--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,12 @@ open System Lake DSL
 
 package leanInk where
   binRoot := `LeanInk.Main
+  dependencies := #[
+    {
+      name := `lake
+      src := Source.git "https://github.com/leanprover/lake.git" "ba9005829cf53e77cb241fc6fb86a2d4e8d8219c"
+    }
+  ]
   moreLinkArgs :=
     if Platform.isWindows then
       #["-Wl,--export-all"]
