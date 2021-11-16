2ef8b5fa1ac9d3aacd6e532d28e554d0c00d3bcf
Niklas Bülow <me@niklasbuelow.com>
2021-11-16 01:46:38 +0100

    fix: implement ToJson manually for Fragment

--- LeanInk/Output/AlectryonFragment/Basic.lean
+++ LeanInk/Output/AlectryonFragment/Basic.lean
@@ -38,4 +38,32 @@ structure Text where
 inductive Fragment where
   | text (value : Text) : Fragment
   | sentence (value : Sentence) : Fragment
-  deriving FromJson, ToJson
+
+namespace Json
+
+/-- 
+We need a custom ToJson implementation for Alectryons fragments.
+
+For example we have following fragment:
+```
+Fragment.text { contents := "Test" }
+```
+
+We want to serialize this to:
+```
+[{"contents": "Test", "_type": "text"}]
+```
+
+instead of:
+```
+[{"text": {"contents": "Test", "_type": "text"}}]
+```
+
+This is because of the way Alectryon decodes the json files. It uses the _type field to
+determine the namedTuple type with Alectryon.
+-/
+instance : ToJson Fragment where
+  toJson
+    | Fragment.text v => toJson v
+    | Fragment.sentence v => toJson v
+  
