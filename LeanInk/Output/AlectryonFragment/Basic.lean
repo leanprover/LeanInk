a917044bc0d11b29a3d01f3cbec57f23baa08552
Niklas Bülow <me@niklasbuelow.com>
2021-11-16 01:46:03 +0100

    feat: add _type field in fragments

--- LeanInk/Output/AlectryonFragment/Basic.lean
+++ LeanInk/Output/AlectryonFragment/Basic.lean
@@ -1,4 +1,5 @@
 import Lean.Data.Json
+import Lean.Data.Json.FromToJson
 
 namespace LeanInk.Output.AlectryonFragment
 
@@ -11,22 +12,26 @@ structure Hypothesis where
   deriving FromJson, ToJson
 
 structure Goal where
+  _type : String := "goal"
   name : String
   conclusion : String
   hyptheses : Array Hypothesis
   deriving FromJson, ToJson
 
 structure Message where
+  _type : String := "message"  
   contents : String
   deriving FromJson, ToJson
 
 structure Sentence where
+  _type : String := "sentence"
   contents : String
   messages : Array Message
   goals : Array Goal
   deriving FromJson, ToJson
 
 structure Text where
+  _type : String := "text"
   contents : String
   deriving FromJson, ToJson
 
