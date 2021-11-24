b55a501e5643a5eb2016a77ab4815051c6e8127d
Niklas Bülow <me@niklasbuelow.com>
2021-11-24 16:43:23 +0100

    chore: remove no longer needed lake dependency

--- lakefile.lean
+++ lakefile.lean
@@ -3,12 +3,6 @@ open System Lake DSL
 
 package leanInk where
   binRoot := `LeanInk.Main
-  dependencies := #[
-    {
-      name := `lake
-      src := Source.git "https://github.com/leanprover/lake.git" "e957827c21e83101c4ce888f1752845dfdd03403"
-    }
-  ]
   moreLinkArgs :=
     if Platform.isWindows then
       #["-Wl,--export-all"]
