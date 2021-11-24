7af145e397537bf5a329f3fcde42d3cbdd49e16c
Niklas Bülow <me@niklasbuelow.com>
2021-11-24 20:31:14 +0100

    feat: resolve goals at tactic fragment

--- LeanInk/Commands/Analyze/Annotation.lean
+++ LeanInk/Commands/Analyze/Annotation.lean
@@ -3,9 +3,27 @@ import LeanInk.Commands.Analyze.InfoTreeUtil
 
 import LeanInk.Output.AlectryonFragment
 
+import Lean.MetavarContext
+import Lean.Elab.InfoTree
+
 namespace LeanInk.Commands.Analyze
 
 open Output.AlectryonFragment
+open Lean
+open Lean.Elab
+
+namespace TacticFragment
+  private def resolveGoalsAux (ctx: ContextInfo) (mctx : MetavarContext) : List MVarId -> IO (List Format)
+    | [] => []
+    | goals => do
+      let ctx := { ctx with mctx := mctx }
+      return [← ctx.runMetaM {} (return Std.Format.prefixJoin "\n" (← goals.mapM (Meta.ppGoal .)))]
+
+  def resolveGoals (self : TacticFragment) : IO (List Goal) := do
+    let goalsBefore ← resolveGoalsAux self.ctx self.info.mctxBefore self.info.goalsBefore
+    return ← goalsBefore.map (λ g => { name := "", conclusion := s!"{g}", hypotheses := #[] } )
+
+end TacticFragment
 
 def annotateFileAux (l : List Fragment) (contents : String) (pos : String.Pos) (f : List TacticFragment) : IO (List Fragment) := do
   IO.println s!"Running Annotation (l: {l.length}) (pos: {pos}) (f: {f.length})"
@@ -17,20 +35,11 @@ def annotateFileAux (l : List Fragment) (contents : String) (pos : String.Pos) (
     -- We don't have any further tactics to annotate so we just return the rest of the contents as a text fragment.
     | [] => l.append [Fragment.text { contents := contents.extract pos contents.length }]
     | t::ts =>
-      let format ← t.toFormat
-      let goal : Goal := { name := "", conclusion := s!"{format}", hypotheses := #[] }
-      let fragment := Fragment.sentence { contents := contents.extract t.headPos t.tailPos, messages := #[], goals := #[goal] }
+      let fragment := Fragment.sentence { contents := contents.extract t.headPos t.tailPos, messages := #[], goals := (← t.resolveGoals).toArray }
       if t.headPos > pos then
         let textFragment := Fragment.text { contents := contents.extract pos t.headPos }
         return ← annotateFileAux (l.append [textFragment, fragment]) contents t.tailPos ts
       else
         return ← annotateFileAux (l.append [fragment]) contents t.tailPos ts
 
-def annotateFile (config: Configuration) (annotations: List TacticFragment) : IO (List Fragment) := do
-  for tactic in annotations do
-    let format ← tactic.toFormat
-    IO.println s!"{format}"
-
-  IO.println s!"{config.inputFileContents}"
-  
-  return ← annotateFileAux [] config.inputFileContents 0 annotations
+def annotateFile (config: Configuration) (annotations: List TacticFragment) : IO (List Fragment) := annotateFileAux [] config.inputFileContents 0 annotations
