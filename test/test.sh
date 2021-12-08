ad2281683779148a8e7e319ba21ef1bea3904fd0
Niklas Bülow <me@niklasbuelow.com>
2021-12-08 19:11:04 +0100

    fix: leanInk path in shell script

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,10 @@
-set -euo pipefail
-
 ulimit -s 8192
 DIFF=diff
 if diff --color --help >/dev/null 2>&1; then
     DIFF="diff --color";
 fi
+
+LEANINK="../build/bin/leanInk"
 
 function error {
     echo $1
@@ -22,10 +22,10 @@ function run_leanink {
     # If the folder in which the file resides contains a lakefile we will use it to resolve any dependencies for the file
     if [[ -f "$directory/lakefile.lean" ]]; then
         echo "Running LeanInk with lakefile.lean - $file"
-        (cd $directory && leanInk analyze $filename --lake lakefile.lean || error "LeanInk failed - $file")
+        (cd $directory && LEANINK analyze $filename --lake lakefile.lean || error "LeanInk failed - $file")
     else
         echo "Running LeanInk - $file"
-        (cd $directory && leanInk analyze $filename || error "LeanInk failed - $file")
+        (cd $directory && LEANINK analyze $filename || error "LeanInk failed - $file")
     fi
 }
 
@@ -37,6 +37,9 @@ function run_tests {
             if test "$file.leanInk.expected"; then
                 run_leanink $file
                 if $DIFF -au --strip-trailing-cr "$file.leanInk" "$file.leanInk.expected"; then
+                    echo "SUCCESSFULL! ($file)"
+                else
+                    echo "FAILED! ($file)"
                     retVal=1
                 fi
             else
