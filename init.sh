e81e7c7d06dc834afcb2bb0d2fdccfc3eae37dfb
Niklas Bülow <me@niklasbuelow.com>
2021-12-15 18:50:48 +0100

    fix: init.sh wrong working directory

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ cd leanInk
 git fetch --tags -q
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $latestTag -q
+sh install.sh
 cd ..
-sh leanInk/install.sh
 rm -rf ./leanInk
