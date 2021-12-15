36df99e5c6d529b3bf0aaadee45969dc159241c3
Niklas Bülow <me@niklasbuelow.com>
2021-12-15 18:13:59 +0100

    refactor: remove old version command implementation

--- LeanInk/Commands/Version.lean
+++ LeanInk/Commands/Version.lean
@@ -9,13 +9,6 @@ def leanVersion : String :=
   else
     versionString ++ s!"-unknown"
 
-def version := s!"0.0.1"
-def cliVersionOutput := s!"LeanInk ({version}) for Lean ({leanVersion})"
-
-def printVersion : IO UInt32 := do
-  IO.println cliVersionOutput
-  return 0
-
 def printLeanVersion : IO UInt32 := do
   IO.println leanVersion
   return 0
