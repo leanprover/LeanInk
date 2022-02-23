5b48121cc07b5e3a5c63e7e6f34232356aa015aa
Niklas Bülow <me@niklasbuelow.com>
2022-02-23 19:08:45 +0100

    chore: update lean-toolchain

--- LeanInk.lean
+++ LeanInk.lean
@@ -1,4 +1,9 @@
 import LeanInk.CLI
-import LeanInk.Commands
+import LeanInk.Analysis
 import LeanInk.Main
-import LeanInk.Output
+import LeanInk.Annotation
+import LeanInk.Configuration
+import LeanInk.FileHelper
+import LeanInk.ListUtil
+import LeanInk.Logger
+import LeanInk.Version
