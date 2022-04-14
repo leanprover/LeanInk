1830775d2fb10485e8c4f0914cdc128aafe47af7
Xubai Wang <self@xubai.wang>
2022-04-14 21:26:40 +0800

    refactor: move Main to top level

--- LeanInk.lean
+++ LeanInk.lean
@@ -1,6 +1,5 @@
 import LeanInk.CLI
 import LeanInk.Analysis
-import LeanInk.Main
 import LeanInk.Annotation
 import LeanInk.Configuration
 import LeanInk.FileHelper
--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,6 @@ import Lake
 open System Lake DSL
 
 package leanInk where
-  binRoot := `LeanInk.Main
   moreLinkArgs :=
     if Platform.isWindows then
       #[]
