83efe2a0411765eda89f6c6e5469cb0d98d4c9ff
Sebastian Ullrich <sebasti@nullri.ch>
2022-06-16 17:20:13 +0200

    test: pin mathlib4

--- test/dep/lakefile.lean
+++ test/dep/lakefile.lean
@@ -5,6 +5,6 @@ package dep {
   defaultFacet := PackageFacet.oleans
   dependencies := #[{
     name := `mathlib
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "master"
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "84ca33e940678954dc146f732f447ddc8984cf39"
   }]
 }
