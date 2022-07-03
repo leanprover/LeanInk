4b5e606ea8cc54c2447ce48706f8ec1d133d19e9
Sebastian Ullrich <sebasti@nullri.ch>
2022-07-03 17:54:50 +0200

    fix: adapt test suite to new Lake, mathlib not compatible yet though

--- test/dep/lakefile.lean
+++ test/dep/lakefile.lean
@@ -1,10 +1,8 @@
 import Lake
 open Lake DSL
 
-package dep {
-  defaultFacet := PackageFacet.oleans
-  dependencies := #[{
-    name := `mathlib
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "84ca33e940678954dc146f732f447ddc8984cf39"
-  }]
-}
+package dep
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
+
+lean_lib dep
