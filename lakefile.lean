549c91fd6a6152c7b466b03f789af93f42ed9a02
Chris Lovett <clovett@microsoft.com>
2022-08-30 12:31:18 -0700

    fix: code review feedback from Mac.

--- lakefile.lean
+++ lakefile.lean
@@ -1,19 +1,12 @@
 import Lake
 open System Lake DSL
 
-package leanInk where
-  moreLinkArgs :=
-    if Platform.isWindows then
-      #[]
-    else
-      #["-rdynamic"]
-      
-      
-lean_lib LeanInk {
-  -- add library configuration options here
-}
+package leanInk
+
+lean_lib LeanInk
 
 @[defaultTarget]
 lean_exe leanInk {
   root := `Main
+  supportInterpreter := true
 }
