d35a9ea66f48c91d5e1cd80394153bcf16cb174a
Chris Lovett <clovett@microsoft.com>
2022-09-02 09:40:57 -0700

    fix: install leanink somewhere else - do not hijack elan install location.

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 import Init.System.IO
 open System Lake DSL
-open System.FilePath
+open System.FilePath IO IO.FS
 
 package leanInk
 
@@ -16,7 +16,7 @@ lean_exe leanInk {
 def getLeanInkExePath : IO (FilePath × FilePath) := do
   let leanInk : FilePath := "build" / "bin" / "leanInk"
   let leanInkExe := leanInk.withExtension FilePath.exeExtension
-  let realPath ← IO.FS.realPath leanInkExe
+  let realPath ← realPath leanInkExe
   return (leanInkExe, realPath)
 
 /-! Run the leanInk that is built locally to analyze the given test file.
@@ -25,8 +25,8 @@ def runLeanInk (test : FilePath) : IO UInt32 := do
   let (leanInkExe, realPath) ← getLeanInkExePath
 
   if ! (← leanInkExe.pathExists) then
-    IO.println s!"Could not find leanInk executable at {leanInkExe}"
-    IO.println s!"Please run `lake build` in the LeanInk directory"
+    println s!"Could not find leanInk executable at {leanInkExe}"
+    println s!"Please run `lake build` in the LeanInk directory"
     return 1
 
   if let some fileName := test.fileName then
@@ -34,27 +34,27 @@ def runLeanInk (test : FilePath) : IO UInt32 := do
     if let some dir := test.parent then
       let lakefile := dir / "lakefile.lean"
       if (← lakefile.pathExists) then
-        IO.println s!"Running test {test} using lake..."
+        println s!"Running test {test} using lake..."
         args := args ++ #["--lake", "lakefile.lean"]
       else
-        IO.println s!"Running test {test}..."
+        println s!"Running test {test}..."
 
-    let out ← IO.Process.output { cmd := realPath.normalize.toString, args := args, cwd := test.parent }
+    let out ← Process.output { cmd := realPath.normalize.toString, args := args, cwd := test.parent }
     if out.exitCode = 0 then
       return 0
     else
-      IO.println s!"leanInk failed with {out.stdout} {out.stderr}"
+      println s!"leanInk failed with {out.stdout} {out.stderr}"
       return out.exitCode
   return 1
 
 /-! Compare the text contents of two files -/
 def runDiff (actual : FilePath) (expected : FilePath) : IO Bool := do
-  let actualStr ← IO.FS.readFile actual
-  let expectedStr ← IO.FS.readFile expected
+  let actualStr ← FS.readFile actual
+  let expectedStr ← FS.readFile expected
   return actualStr.trim = expectedStr.trim
 
 def copyFile (src : FilePath) (dst : FilePath) : IO Unit := do
-  IO.FS.writeBinFile dst (← IO.FS.readBinFile src)
+  FS.writeBinFile dst (← FS.readBinFile src)
 
 def indexOf? [BEq α] (xs : List α) (s : α) (start := 0): Option Nat :=
   match xs with
@@ -63,15 +63,14 @@ def indexOf? [BEq α] (xs : List α) (s : α) (start := 0): Option Nat :=
 
 def getTargetLeanInkPath : IO (Option FilePath) := do
   let (leanInkExe, _) ← getLeanInkExePath
-  let lakePath ← IO.appPath
-  let index := indexOf? lakePath.components ".elan"
-  if let some i := index then
-    if let some fileName := leanInkExe.fileName then
-      let elanPath := lakePath.components.take (i + 1)
-      let sep := pathSeparator.toString
-      let elanPath : FilePath := sep.intercalate elanPath
-      let leanInkPath := elanPath / "bin" / fileName
-      return some leanInkPath
+  if let some fileName := leanInkExe.fileName then
+    let loc := if System.Platform.isWindows then "LOCALAPPDATA" else "HOME"
+    let val? ← getEnv loc
+    if let some homePath := val? then
+      let targetPath : FilePath := homePath / ".leanink" / "bin"
+      if !(← targetPath.pathExists) then
+        createDirAll targetPath
+      return some (targetPath / fileName)
   return none
 
 /-! Walk the `test` folder looking for every `.lean` file that's not a `lakefile` or part of an
@@ -92,45 +91,52 @@ def execute (capture : Bool) : IO UInt32 := do
           if rc ≠ 0 then
             return 1
           else if (capture) then
-            IO.println s!"  UPDATING {expected}"
+            println s!"  UPDATING {expected}"
             copyFile actual expected
           else
             if (← runDiff actual expected) then
-              IO.println s!"  SUCCESS"
+              println s!"  SUCCESS"
             else
-              IO.println s!"  FAILED: diff {expected} {actual}"
+              println s!"  FAILED: diff {expected} {actual}"
               retVal := retVal + 1
         else
-          IO.println s!"  FAILED: expected output file is missing: {expected}"
+          println s!"  FAILED: expected output file is missing: {expected}"
           retVal := retVal + 1
 
   if retVal > 0 then
-    IO.println s!"FAILED: {retVal} tests failed!"
+    println s!"FAILED: {retVal} tests failed!"
   return retVal
 
 script tests (args) do
-  IO.println "Running diff tests for leanInk"
+  if args.length > 0 then
+    println s!"Unexpected arguments: {args}"
+  println "Running diff tests for leanInk"
   execute False
 
 script capture (args) do
-  IO.println "Updating .leanInk.expected output files"
+  if args.length > 0 then
+    println s!"Unexpected arguments: {args}"
+  println "Updating .leanInk.expected output files"
   execute True
 
 script install (args) do
-  IO.println "Installing leanInk..."
-  IO.println "  Building leanInk..."
+  if args.length > 0 then
+    println s!"Unexpected arguments: {args}"
   if let some targetPath ← getTargetLeanInkPath then
+    println s!"Installing leanInk to {targetPath}"
+    println "Please ensure this location is in your PATH"
     let (_, realPath) ← getLeanInkExePath
-    let out ← IO.Process.output { cmd := "lake", args := #["build"] }
+    println "  Building leanInk..."
+    let out ← Process.output { cmd := "lake", args := #["build"] }
       if out.exitCode = 0 then
-        IO.println s!"  Built {realPath}..."
-        IO.println s!"  Installing {targetPath}..."
+        println s!"  Built {realPath}..."
+        println s!"  Copying to {targetPath}..."
         copyFile realPath targetPath
-        IO.println s!"  LeanInk successfully installed!"
+        println s!"  LeanInk successfully installed!"
         return 0
       else
-        IO.println s!"### Failed to build leanInk:\n{out.stdout} {out.stderr}"
+        println s!"### Failed to build leanInk:\n{out.stdout} {out.stderr}"
         return out.exitCode
   else
-    IO.println "### Cannot find .elan target directory"
+    println "### Cannot find .elan target directory"
     return 1
