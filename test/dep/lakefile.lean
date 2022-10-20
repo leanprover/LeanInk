12611ecee71086bdabd607032cbfeb1f19d8ae29
Gabriel Ebner <gebner@gebner.org>
2022-10-20 07:55:55 -0700

    fix: tests

--- test/dep/lakefile.lean
+++ test/dep/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 package dep
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "362fc2dcb0b9136ac644caa493ed70ab15a5cfa8"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
-@[defaultTarget]
+@[default_target]
 lean_lib dep
