927c8e7c5af0f7397c4f39ffc4836cb7dc00cb81
ART0 <18333981+0Art0@users.noreply.github.com>
2023-05-04 22:26:35 +0530

    Fix bug

--- LeanInk/Annotation/Basic.lean
+++ LeanInk/Annotation/Basic.lean
@@ -12,4 +12,29 @@ namespace LeanInk.Annotation
 
 open LeanInk.Analysis
 
-def annotateFile (analysis : AnalysisResult) : AnalysisM (List Annotation) := pure []
+/-
+  Annotation
+-/
+def tokensBetween (head : String.Pos) (tail : Option String.Pos) (compounds: List (Compound Token)) : List (Compound Token) := Id.run do
+  let mut tokens : Array (Compound Token) := #[]
+  for token in compounds do
+    match (tail, token.tailPos) with
+    | (_, none) => continue
+    | (some tail, some tokenTail) =>
+      if token.headPos <= tail && tokenTail > head then
+        tokens ← tokens.push token
+    | (none, some tokenTail) => 
+      if tokenTail > head then
+        tokens ← tokens.push token
+  return tokens.toList
+
+def matchTokenToAnalysis (aux : List Annotation) : List (Compound Sentence) -> List Annotation
+  | [] => aux
+  | x::y::xs =>
+    matchTokenToAnalysis (aux.append [{ sentence := x }]) (y::xs)
+  | x::xs =>
+    matchTokenToAnalysis (aux.append [{ sentence := x }]) xs
+
+def annotateFile (analysis : AnalysisResult) : AnalysisM (List Annotation) := do
+  let compounds ← matchCompounds (toFragmentIntervals analysis.sentences)
+  return matchTokenToAnalysis [] compounds
