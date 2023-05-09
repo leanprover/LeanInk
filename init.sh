148315b15fa597e6017ba60f8a27099bdff2da3f
ART0 <18333981+0Art0@users.noreply.github.com>
2023-05-09 22:28:24 +0530

    Delete `init.sh`

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/insightmind/LeanInk ./leanInk -q
-cd leanInk
-git fetch --tags -q
-latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
-git checkout $latestTag -q
-lake script run install && cd .. && rm -rf ./leanInk
