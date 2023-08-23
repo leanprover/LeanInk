c4d656aaad9745b017b21e9ecf0dae826f28596a
Scott Morrison <scott.morrison@gmail.com>
2023-08-23 14:54:37 +1000

    chore: bump to nightly-2023-04-20

--- test/dep/lakefile.lean
+++ test/dep/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 package dep
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "1cd8b316f3e3e2f1e307b4e38e2b304ae5e596bd"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "408fbe22495657efd034398efa252f3f2b33add1"
 
 @[default_target]
 lean_lib dep
