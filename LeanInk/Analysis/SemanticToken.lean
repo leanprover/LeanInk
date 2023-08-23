77dd55951a51b19e0f3e371cf37fead18227de18
Scott Morrison <scott.morrison@gmail.com>
2023-08-23 14:26:31 +1000

    chore: clean up unused arguments

--- LeanInk/Analysis/SemanticToken.lean
+++ LeanInk/Analysis/SemanticToken.lean
@@ -30,8 +30,8 @@ namespace SemanticTraversalInfo
     | some node => do
       if Info.isExpanded node then
         return []
-      match info.stx.getRange?, info.node with
-      | some range, Info.ofTermInfo info => do
+      match info.node with
+      | Info.ofTermInfo info => do
         let genToken := genSemanticToken info.stx
         match info.expr with
         | Expr.fvar .. => return genToken SemanticTokenType.variable
@@ -43,11 +43,11 @@ namespace SemanticTraversalInfo
             else
               return []
           | _ => pure []
-      | _, _ => pure []
+      | _ => pure []
     | _ => pure []
 
-  def highlightKeyword (headPos tailPos : String.Pos) (stx: Syntax) : AnalysisM (List Token) := do
-    if let Syntax.atom info val := stx then
+  def highlightKeyword (stx: Syntax) : AnalysisM (List Token) := do
+    if let Syntax.atom _ val := stx then
       if (val.length > 0 && val.front.isAlpha) || (val.length > 1 && val.front = '#' && (val.get ⟨1⟩).isAlpha) then
         return genSemanticToken stx SemanticTokenType.keyword
     return []
@@ -66,7 +66,7 @@ namespace SemanticTraversalInfo
       | `($id:ident) => highlightIdentifier tailPos { info with stx := id }
       | _ => do
         if !(FileWorker.noHighlightKinds.contains stx.getKind) then
-          let token ← highlightKeyword headPos tailPos stx
+          let token ← highlightKeyword stx
           if stx.isOfKind choiceKind then
             _resolveSemanticTokens (aux ++ token) { info with stx := stx[0] }
           else
