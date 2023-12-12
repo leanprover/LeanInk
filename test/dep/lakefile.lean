71b8365e08fb8b786cff628bbea38a2512370ca7
utensil <utensilcandel@gmail.com>
2023-12-12 18:31:01 +0800

    chore: bump tests to v4.4.0-rc1 and lake update

--- test/dep/lakefile.lean
+++ test/dep/lakefile.lean
@@ -3,7 +3,5 @@ open Lake DSL
 
 package dep
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "cf479f4ee9e6e5e54f8b3ddea0011c3f816350d4"
-
-@[default_target]
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
 lean_lib dep
